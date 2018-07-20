7fc9ef438575b624a5c2f3a1edd140cf3c9d3016
ywangnon <ywangnon123@gmail.com>
2018-07-20 22:05:38 +0900

    추가

--- Programmers/이상한문자만들기.playground/Contents.swift
+++ Programmers/이상한문자만들기.playground/Contents.swift
@@ -87,3 +87,28 @@ func solution3(_ s:String) -> String {
     return ans
 }
 
+print("\n---------- [ 다른 사람의 풀이 ] ----------\n")
+
+func solution4(_ s:String) -> String {
+    var check = true
+    var result = ""
+    for i in s
+    {
+        if(i == " ")
+        {
+            result += String(i)
+            check = true
+        }else{
+            if(check)
+            {
+                result += String(i).uppercased()
+                check = false
+            }else{
+                result += String(i).lowercased()
+                check = true
+            }
+        }
+    }
+    return result
+}
+
